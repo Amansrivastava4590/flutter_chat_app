6ca0e5acc89d43879ed7c236bd39a6f4879462fb
Aman Srivastava <aman.srivastava@satat.tech>
2023-12-15 18:07:40 +0530

    chats page done

--- lib/global.dart
+++ lib/global.dart
@@ -9,7 +9,7 @@ import 'package:http/http.dart' as http;
 import 'model/me_model.dart';
 import 'model/message_model.dart';
 
-const url = "http://localhost:5000";
+const url = "http://192.168.1.96:3000";
 
 class AppColors {
   static Color? primary = const Color(0xFFC10000);
@@ -19,25 +19,27 @@ class Whatsapp {
   static const whatsappUrl = "$url/whatsapp";
 
   static Future<meModel> Me()async{
-    final res = await http.get(Uri.parse("$whatsappUrl/me"));
+    final res = await http.get(Uri.parse("$whatsappUrl-users/me"));
     if(res.statusCode == 200){
+      print(res.body);
       return meModel.fromJson(json.decode(res.body));
     }
     throw Exception(res.reasonPhrase);
 
   }
   static Future<List<ChatModel>> Chats()async{
-    final res = await http.get(Uri.parse("$whatsappUrl/chats"));
+    final res = await http.get(Uri.parse("$whatsappUrl-chats/chats"));
     //Response response = await get(Uri.parse("$whatsappUrl/chats"));
     if(res.statusCode == 200){
       final parsed = jsonDecode(res.body);
+      print(parsed);
       return parsed.map<ChatModel>((json) => ChatModel.fromJson(json)).toList();
     }
     throw Exception(res.reasonPhrase);
 
   }
   static Future<List<CallModel>> Call()async{
-    final res = await http.get(Uri.parse("$whatsappUrl/calls"));
+    final res = await http.get(Uri.parse("$whatsappUrl-calls/calls"));
     //Response response = await get(Uri.parse("$whatsappUrl/chats"));
     if(res.statusCode == 200){
       final parsed = jsonDecode(res.body);
@@ -48,7 +50,7 @@ class Whatsapp {
   }
   static Future<List<PeopleModel>> People()async{
     try {
-      final res = await http.get(Uri.parse("$whatsappUrl/people"),
+      final res = await http.get(Uri.parse("$whatsappUrl-messages/people"),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=utf-8',
           }
